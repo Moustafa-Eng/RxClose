c42c35f9861a340852451bc6351ded819e416d13
walaa <wlaa21511@gmail.com>
2025-03-24 22:34:19 +0200

    ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,23 +5,29 @@ import { RegisterComponent } from './components/register/register.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { BlankLayoutComponent } from './layouts/blank-layout/blank-layout.component';
 import { HomeComponent } from './components/home/home.component';
+import { MostsearchListComponent } from './features/searchpage/components/mostsearch-list/mostsearch-list.component';
+import { ContactpageComponent } from './features/contactuspage/contactpage/contactpage.component';
+import { CartListComponent } from './features/cart/components/cart-list/cart-list.component';
+import { CaregorylistComponent } from './features/category/caregorylist/caregorylist.component';
 
 export const routes: Routes = [
 
 
-  {path: '', redirectTo: 'auth/home', pathMatch: 'full'},
-  {path: 'signin', component: LoginComponent },
-  {path : 'signup', component: RegisterComponent},
+  { path: '', redirectTo: 'auth/home', pathMatch: 'full' },
+  { path: 'signin', component: LoginComponent },
+  { path: 'signup', component: RegisterComponent },
+  { path: 'auth', component: AuthLayoutComponent, children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'search', component: MostsearchListComponent },
+      { path: 'Contactus', component: ContactpageComponent },
+      { path: 'cart', component: CartListComponent },
+      { path: 'category', component:CaregorylistComponent },
+    ]
+  },
+  {
+    path: 'blank', component: BlankLayoutComponent, children: [
 
-  {path: 'auth', component: AuthLayoutComponent, children: [
-    {path: 'home', component: HomeComponent},
-  ]},
-
-
-  {path: 'blank', component: BlankLayoutComponent, children: [
-    
-  ]},
-
-
-  {path: '**', component: NotfoundComponent}
+    ]
+  },
+  { path: '**', component: NotfoundComponent }
 ];
